447902641caf9f995e0dc43f2731fcdd2cf6421d
Volodymyr Bakovskyi <vovabak84@gmail.com>
2022-08-18 19:20:45 +0300

    task 2 started

---
+++ src/js/02-timer.js
@@ -0,0 +1,10 @@
+const refs = {
+    datetimePicker: document.querySelector('#datetime-picker'),
+    startBtn: document.querySelector('[data-start]'),
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]'),
+}
+
+document.body.style.backgroundColor = 'tomato';
