e944bd1f68a41449ec9c7b8f42dd0a7500d5b011
Volodymyr Bakovskyi <vovabak84@gmail.com>
2022-08-18 18:51:14 +0300

    task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,34 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+}
+
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+
+refs.stopBtn.setAttribute('disabled', true);
+let intervalId = null;
+
+function onStart() {
+    changeColor();
+    refs.startBtn.setAttribute('disabled', true);
+    refs.stopBtn.removeAttribute('disabled');
+    intervalId = setInterval(changeColor, 1000);
+}
+
+function onStop() {
+    refs.startBtn.removeAttribute('disabled');
+    refs.stopBtn.setAttribute('disabled', true);
+    clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
 
 
