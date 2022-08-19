19bd8e3fb23408f825841d53e60bbea2af45304b
vovabak <50016109+vovabak@users.noreply.github.com>
2022-08-19 08:56:34 +0000

    Deploying to gh-pages from @ vovabak/goit-js-hw-09@9dfcf1e65230a974bca82780d8b235d7a3706550 🚀

--- 02-timer.a67f25f5.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#datetime-picker"),document.querySelector("[data-start]"),document.querySelector("[data-days]"),document.querySelector("[data-hours]"),document.querySelector("[data-minutes]"),document.querySelector("[data-seconds]");document.body.style.backgroundColor="tomato";
-//# sourceMappingURL=02-timer.a67f25f5.js.map
